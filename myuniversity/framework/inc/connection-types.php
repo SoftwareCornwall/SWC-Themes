526ef7581452dbb914c6edfaa302eeb1d963ad21
Tony Edwards <tony@plymouthsoftware.com>
2018-01-07 16:07:38 +0000

    Removes connection type between professiors and courses
    An error was throws up when editing a page as the post to post plugin was looking for a connection which no longer exists.

--- myuniversity/framework/inc/connection-types.php
+++ myuniversity/framework/inc/connection-types.php
@@ -7,14 +7,14 @@
 
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-if ( is_plugin_active( 'posts-to-posts/posts-to-posts.php' ) ) {
-	function sd_connection_types() {
-	    p2p_register_connection_type( array(
-    	    'name' => 'professors_to_courses',
-        	'from' => 'professors',
-	        'to' => 'courses'
-    	) );
-	}
-	add_action( 'p2p_init', 'sd_connection_types' );
-}
+// if ( is_plugin_active( 'posts-to-posts/posts-to-posts.php' ) ) {
+// 	function sd_connection_types() {
+// 	    p2p_register_connection_type( array(
+//     	    'name' => 'professors_to_courses',
+//         	'from' => 'professors',
+// 	        'to' => 'courses'
+//     	) );
+// 	}
+// 	add_action( 'p2p_init', 'sd_connection_types' );
+// }
 ?>
