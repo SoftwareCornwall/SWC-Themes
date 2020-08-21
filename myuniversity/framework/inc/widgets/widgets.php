4595635cc28186eef3b12602fe7c61a2c16ec6b9
Tony Edwards <tony.edwards@gmail.com>
2020-08-21 19:55:53 +0100

    Removes Flickr widget

--- myuniversity/framework/inc/widgets/widgets.php
+++ myuniversity/framework/inc/widgets/widgets.php
@@ -5,5 +5,4 @@
 
 require_once( SD_FRAMEWORK_INC . 'widgets/recent-posts.php' );
 require_once( SD_FRAMEWORK_INC . 'widgets/popular-posts.php' );
-require_once( SD_FRAMEWORK_INC . 'widgets/flickr.php' );
 require_once( SD_FRAMEWORK_INC . 'widgets/social-icons.php' );
