ad61ab2338d255c7fb9a04283d571b018bf337ab
Tony Edwards <tony.edwards@gmail.com>
2020-08-21 18:37:23 +0100

    Remove hardcoded ReCaptcha

--- myuniversity/functions.php
+++ myuniversity/functions.php
@@ -13,48 +13,6 @@ add_filter('option_users_can_register', function($value) {
 });
 
 
-/**
- * add recaptcha to job post and resume post belinda oct17
- */
-
-// Define your keys here
-define( 'RECAPTCHA_SITE_KEY',   '6LdqVzUUAAAAAIAZRYEm3MiJUHtqSAAio-1KgK-R' );
-define( 'RECAPTCHA_SECRET_KEY', '6LdqVzUUAAAAAGkHEyAceXoN6DAkev1s8xQejEXU' );
-// Enqueue Google reCAPTCHA scripts
-add_action( 'wp_enqueue_scripts', 'recaptcha_scripts' );
-function recaptcha_scripts() {
-    wp_enqueue_script( 'recaptcha', 'https://www.google.com/recaptcha/api.js' );
-}
-// Add reCAPTCHA to the job submission form
-// If you disabled company fields, the submit_job_form_end hook can be used instead from version 1.24.1 onwards
-add_action( 'submit_job_form_company_fields_end', 'recaptcha_field' );
-function recaptcha_field() {
-?>
-<fieldset>
-    <label>Are you human?</label>
-    <div class="field">
-        <div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITE_KEY; ?>"></div>
-    </div>
-</fieldset>
-<?php
-}
-// Validate
-add_filter( 'submit_job_form_validate_fields', 'validate_recaptcha_field' );
-function validate_recaptcha_field( $success ) {
-$response = wp_remote_get( add_query_arg( array(
-'secret'   => RECAPTCHA_SECRET_KEY,
-'response' => isset( $_POST['g-recaptcha-response'] ) ? $_POST['g-recaptcha-response'] : '',
-'remoteip' => isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']
-), 'https://www.google.com/recaptcha/api/siteverify' ) );
-if ( is_wp_error( $response ) || empty( $response['body'] ) || ! ( $json = json_decode( $response['body'] ) ) || ! $json->success ) {
-return new WP_Error( 'validation-error', '"Are you human" check failed. Please try again.' );
-}
-return $success;
-}
-add_action( 'submit_resume_form_resume_fields_end', 'recaptcha_field' );
-add_filter( 'submit_resume_form_validate_fields', 'validate_recaptcha_field' );
-
-
 /*-----------------------------------------------------------------------------------*/
 /* Define Theme Constants
 /*-----------------------------------------------------------------------------------*/
