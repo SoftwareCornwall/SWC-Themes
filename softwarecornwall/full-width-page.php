965e89b0c750d8a9fed8460f3879e1dd403af855
Tony Edwards <tony.edwards@gmail.com>
2020-08-23 23:34:35 +0100

    Adds bootstap grid around full page layout

--- softwarecornwall/full-width-page.php
+++ softwarecornwall/full-width-page.php
@@ -2,24 +2,38 @@
 /* ------------------------------------------------------------------------ */
 /* Template Name: Page: Full Width
 /* ------------------------------------------------------------------------ */
+// Template Name: Page: Full Width
 get_header();
-
 ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'sd-full-width clearfix' ); ?>> 
-	
-	<!-- entry content -->
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div>
-	<!-- entry content end--> 
-</article>
-<!--post-end-->
 
-<?php endwhile; else: ?>
-<p>
-	<?php _e( 'Sorry, no posts matched your criteria', 'sd-framework' ) ?>
-	.</p>
-<?php endif; ?>
-<!-- content end -->
+<div class="sd-blog-page">
+	<div class="container">
+		<div class="row">
+			<!--left col-->
+			<div class="col-sm-12>
+
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<article id=" post-<?php the_ID(); ?>" <?php post_class( 'sd-full-width clearfix' ); ?>>
+
+				<!-- entry content -->
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+				<!-- entry content end-->
+				</article>
+				<!--post-end-->
+
+				<?php endwhile; else: ?>
+				<p>
+					<?php _e( 'Sorry, no posts matched your criteria', 'sd-framework' ) ?>
+					.</p>
+				<?php endif; ?>
+				<!-- content end -->
+
+
+			</div>
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
