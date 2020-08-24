6c02782c4abef47a8d6597c9e825148c45cda107
Tony Edwards <tony.edwards@gmail.com>
2020-08-24 12:55:33 +0100

    Removes HTML compatability JS files to CDN

--- softwarecornwall/header.php
+++ softwarecornwall/header.php
@@ -44,9 +44,10 @@ global $sd_data;
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!-- Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. -->
 <!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/framework/js/html5.js" type="text/javascript"></script>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 <?php wp_head(); ?>
 <meta name="google-site-verification" content="QtMFJSyGc0UZN0WKYBgMmeXVMYlBGJZOYohqQLxi32M" />
