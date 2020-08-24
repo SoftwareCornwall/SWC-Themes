b26e473f9e8417d2beac16ba6580d17579b71ed8
Tony Edwards <tony.edwards@gmail.com>
2020-08-24 12:48:08 +0100

    Disables and removes mentions to comments

--- softwarecornwall/functions.php
+++ softwarecornwall/functions.php
@@ -324,4 +324,23 @@ function modify_footer_admin () {
 }
 add_filter('admin_footer_text', 'modify_footer_admin');
 
+// Removes from post and pages
+add_action('init', 'remove_comment_support', 100);
+function remove_comment_support() {
+    remove_post_type_support( 'post', 'comments' );
+    remove_post_type_support( 'page', 'comments' );
+}
+
+// Remove comments from the admin area
+add_action( 'admin_menu', 'my_remove_admin_menus' );
+function my_remove_admin_menus() {
+    remove_menu_page( 'edit-comments.php' );
+}
+
+function software_cornwall_admin_bar_render() {
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('comments');
+}
+add_action( 'wp_before_admin_bar_render', 'software_cornwall_admin_bar_render' );
+
 ?>
