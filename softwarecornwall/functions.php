79c932b955d0ea4484a74c640b23724d8d1c6d77
Tony Edwards <tony.edwards@gmail.com>
2020-08-24 13:14:09 +0100

    Removes REST API links from markup

--- softwarecornwall/functions.php
+++ softwarecornwall/functions.php
@@ -327,4 +327,9 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+// Hide WP REST API links in page headers
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10);
+remove_action( 'template_redirect', 'rest_output_link_header', 11);
+
+
 ?>
