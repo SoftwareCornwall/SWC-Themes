49aa476feda618f88698611575efa495623a9beb
Tony Edwards <tony.edwards@gmail.com>
2020-08-24 13:28:06 +0100

    Removes oembed links from the markup

--- softwarecornwall/functions.php
+++ softwarecornwall/functions.php
@@ -348,4 +348,10 @@ remove_action( 'wp_head', 'wlwmanifest_link');
 
 // Remove Shortlink from the Markup
 remove_action( 'wp_head', 'wp_shortlink_wp_head');
+
+// Removes  some oembed stuff included by default
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
 ?>
