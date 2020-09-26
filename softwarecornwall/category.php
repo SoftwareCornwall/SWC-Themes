c40824f263aa6aee14ce1d89ed3a6dc5fa171a9c
Tony Edwards <tony.edwards@gmail.com>
2020-09-26 14:02:37 +0100

    Update formatting

--- softwarecornwall/category.php
+++ softwarecornwall/category.php
@@ -35,10 +35,12 @@ get_header();
 								<?php endif; ?>
 								<div class="sd-entry-content">
 									<header>
-										<h3 class="sd-entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink la %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
-											<?php the_title(); ?>
-											</a> </h3>
-									<span class="sd-latest-blog-date"><i class="fa fa-pencil"></i> <?php the_time( get_option( 'date_format') ); ?></span>
+										<h3 class="sd-entry-title">
+											<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink la %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+												<?php the_title(); ?>
+											</a>
+										</h3>
+										<span class="sd-latest-blog-date"><i class="fa fa-pencil"></i> <?php the_time( get_option( 'date_format') ); ?></span>
 									</header>
 									<?php the_excerpt(); ?>
 								</div>
