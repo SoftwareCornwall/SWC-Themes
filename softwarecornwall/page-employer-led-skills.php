646fab359f32240595111bf843b4cbe36583f33f
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2024-01-16 20:26:47 +0000

    REmoves EU logo

--- softwarecornwall/page-employer-led-skills.php
+++ softwarecornwall/page-employer-led-skills.php
@@ -97,31 +97,14 @@ get_header();
 					<?php endif; endwhile; endif; wp_reset_postdata(); ?>
 
 					</br></br>
-					<h2>European Social Fund</h2>
-					<p>The European Social Fund is partially funding Software Cornwall to run this training project to provide development training for employees within the software industry. As a Software Cornwall member you will be receiving a discount to this course by email. This course is only available to those from the UK or EU. To participate in the training certain personal information will be required from attendants as proof of employment and eligibility to attend. This information is for the purposes of ensuring that the training will go to the correct participants. You will also be required to complete an evaluation form after the training as part of the attendance.</p>
-					<img 
-						src="https://softwarecornwall.org/wp-content/uploads/2020/10/ESF_logo.png" 
-						alt="European Social Fund Logo" 
-						loading="lazy" 
-						height="74px" width="360px"
-						style="width: 100%;"/>
-					</br></br>
+					
 				</div>
 			</div>
 
 			<!--sidebar-->
 			<div class="col-md-4">
 				<aside id="recent-posts-2" class="sd-sidebar-widget clearfix widget_recent_entries">
-					<div class="sd-title-wrapper">
-						<h3 class="sd-styled-title">Part <span class="sd-light">Financed By:</span></h3>
-					</div>
-					<img 
-						src="https://softwarecornwall.org/wp-content/uploads/2020/10/ESF_logo.png" 
-						alt="European Social Fund Logo" 
-						loading="lazy" 
-						height="74px" width="360px"
-						style="width: 100%;"/>
-					</br></br>
+					
 					<div class="sd-title-wrapper">
 						<h3 class="sd-styled-title">Latest <span class="sd-light">ELS News</span></h3>
 					</div>
